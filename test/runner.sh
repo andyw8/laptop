334b39f6fabf104e997829223a67978092e0affd
patrick brisbin <pbrisbin@gmail.com>
2013-09-13 17:16:53 -0400

    Add initial laptop test using precise

---
+++ test/runner.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+message() {
+  printf "\n\e[1;34m:: \e[1;37m%s\e[0m\n" "$@"
+}
+
+failure() {
+  printf "\e[1;31mFAILURE\e[0m: \e[1;37m%s\e[0m\n\n" "$@" >&2;
+  continue
+}
+
+for vagrantfile in test/Vagrantfile.*; do
+  message "Testing with $vagrantfile"
+
+  ln -sf "$vagrantfile" ./Vagrantfile || failure 'Unable to link Vagrantfile'
+
+  message 'Destroying and recreating virtual machine'
+  vagrant destroy || failure 'Unable to destroy existing VM'
+  vagrant up      || failure 'Unable to start virtual machine'
+
+  # TODO: Create a Vagrantfile.mac that uses VMWare Fusion to run OSX
+  if echo "$vagrantfile" | grep -q '\.mac$'; then
+    vagrant ssh -c 'echo vagrant | zsh /vagrant/mac' \
+      || failure 'Installation script failed to run'
+  else
+    vagrant ssh -c 'echo vagrant | sh /vagrant/linux-prerequisites' \
+      || failure 'Prerequisite script to run'
+
+    vagrant ssh -c 'zsh /vagrant/linux' \
+      || failure 'Installation script failed to run'
+  fi
+
+  test "$(vagrant ssh -c 'echo $SHELL')" -eq '/bin/zsh' \
+    || failure 'Installation did not set $SHELL to ZSH'
+
+  test "$(vagrant ssh -c 'ruby --version')" -eq 'ruby 2.0.0-p247' \
+    || failure 'Installation did not install the correct ruby'
+
+  message "$vagrantfile tested succesffully"
+done
+
+rm -f ./Vagrantfile
